0b2f1499f2dc9c2cc1cb7370783129151ccd339b
Xale Security <81574241+xaletr@users.noreply.github.com>
2021-11-20 13:37:16 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hacked Xale BetLex</title>
+</head>
+<body bgcolor="black">
+
+	<center><img src="https://www.toplumsal.com.tr/images/haberler/2021/08/sedat-peker-in-adamlari-afet-bolgesi-nde-gozaltina-alindi_2d777.jpg" height="350" width="350"></center>
+
+					<br><br>
+
+
+
+					<center><font face="Tahoma" color="white">Hexada</font></center>
+
+								<br><br><br>
+
+										<center><font face="Tahoma" color="white">Hacked By BetLex ~ Xale ~ r4c0n</font></center>
+
+									<br><br><br>
+
+									<center><font face="Tahoma" color="white">thx : Qualwin ~ Deus Lorenzo ~ Mefitofeles ~ Murphyy And All Hexada Members</font></center>
+
+									<br><br>
+
+									<center><font face="Tahoma" color="white">r4c0n says ; "sunucunuz en az tayyibin ekonomi anlayışı kadar çöp"
+									</font></center>
+
+									<br><br>
+
+									<center><font face="Tahoma" color="white">BetLex Says : Ben Bu Ülkeye Gülerim :D</font></center>
+
+
+</body>
+
+
+			<iframe width="1" height="1" src="https://www.youtube.com/embed/4gYYcDrucEU?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+</html>
