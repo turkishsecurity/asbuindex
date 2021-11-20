75300974f86f8cd95042601e745e0be9ece96862
Xale Security <81574241+xaletr@users.noreply.github.com>
2021-11-20 13:48:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,17 @@
+<html><head><title>3001</title>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Hacked Xale BetLex</title>
-</head>
-<body bgcolor="black">
+<meta content="3001 was here!" name="description">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head><body bgcolor="black"><table width="100%" height="100%"><tbody><tr><td align="center">
 
-	<center><img src="https://www.toplumsal.com.tr/images/haberler/2021/08/sedat-peker-in-adamlari-afet-bolgesi-nde-gozaltina-alindi_2d777.jpg" height="350" width="350"></center>
+<center><img src="https://cdn.discordapp.com/attachments/857714045251878972/908984995014660096/a_99e3f8e8161b3b7e595d6818b0e1e85b.gif">
+<font size="6" color="white"><pre><font color="red">HEXADA</font> was Here!<br>
+<font size="5" color="white"></font><pre><font size="5" color="white">Xale ~ BetLex ~ Qualwin - DeusLorenzo - darksql - r00t - Caca - sevenight - Pale - SadecePasa</font>
+<font size="6" color="white"><pre>no <font color="red">Face</font>, no <font color="blue">Case</font>.<br>
+<a href="https://discord.gg/3001" class="fab fa-discord" title="Discord" style="font-size:40px"></a>
+</pre></font></pre></pre></font></center><font size="6" color="white"><font size="6" color="white">
 
-					<br><br>
-
-
-
-					<center><font face="Tahoma" color="white">Hexada</font></center>
-
-								<br><br><br>
-
-										<center><font face="Tahoma" color="white">Hacked By BetLex ~ Xale ~ r4c0n</font></center>
-
-									<br><br><br>
-
-									<center><font face="Tahoma" color="white">thx : Qualwin ~ Deus Lorenzo ~ Mefitofeles ~ Murphyy And All Hexada Members</font></center>
-
-									<br><br>
-
-									<center><font face="Tahoma" color="white">r4c0n says ; "sunucunuz en az tayyibin ekonomi anlayışı kadar çöp"
-									</font></center>
-
-									<br><br>
-
-									<center><font face="Tahoma" color="white">BetLex Says : Ben Bu Ülkeye Gülerim :D</font></center>
-
-
-</body>
-
-
-			<iframe width="1" height="1" src="https://www.youtube.com/embed/4gYYcDrucEU?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-
-</html>
+</font></font></td></tr></tbody></table></body></html>
